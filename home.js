d92a1b42f7b27be79dd6dca3dd4a461908e0a968
Urvin Montero <40257372+urvincarlmontero2502@users.noreply.github.com>
2026-04-09 13:55:33 +0800

    Add files via upload

--- home.js
+++ home.js
@@ -448,3 +448,32 @@ function previewImage(event) {
     reader.readAsDataURL(file)
   }
 }
+
+const scrollContainer = document.querySelector('.filter-bar')
+
+let isDown = false
+let scrollLeft
+let startX
+
+scrollContainer.addEventListener('mousedown', (e) => {
+  isDown = true
+  scrollContainer.classList.add('active')
+  startX = e.pageX - scrollContainer.offsetLeft
+  scrollLeft = scrollContainer.scrollLeft
+})
+
+scrollContainer.addEventListener('mouseleave', () => {
+  isDown = false
+})
+
+scrollContainer.addEventListener('mouseup', () => {
+  isDown = false
+})
+
+scrollContainer.addEventListener('mousemove', (e) => {
+  if (!isDown) return
+  e.preventDefault()
+  const x = e.pageX - scrollContainer.offsetLeft
+  const walk = (x - startX) * 2 // Scroll speed
+  scrollContainer.scrollLeft = scrollLeft - walk
+})
